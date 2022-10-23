31d5acc60f81b8d1faa8417a5ee4150fca161ac0
victordantasdev <victordantasdeveloper@gmail.com>
2022-10-23 17:22:06 -0300

    feat: adds aliases file

---
+++ aliases.sh
@@ -0,0 +1,14 @@
+\n
+# System aliases
+alias i="sudo apt install -y"
+alias update="sudo apt update && sudo apt full-upgrade -y"
+alias ls="exa -lh --icons --group-directories-first"
+
+# Git aliases
+alias gd="git checkout develop"
+alias gm="git checkout master"
+alias gs="git checkout staging"
+alias gc="softa-autocommit"
+alias gk="git checkout"
+
+alias ocaml="rlwrap ocaml"
