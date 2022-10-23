a6f2dce5f9970c81f032303c13654b8686df22f6
victordantasdev <victordantasdeveloper@gmail.com>
2022-10-23 17:21:54 -0300

    feat: adds config file

---
+++ .config/spaceship.zsh
@@ -0,0 +1,47 @@
+SPACESHIP_USER_SHOW=always
+SPACESHIP_DIR_TRUNC_REPO=false
+SPACESHIP_GIT_SHOW=true
+
+SPACESHIP_PROMPT_ORDER=(
+  time      # Time stampts section
+  user      # Username section
+  dir       # Current directory section
+  host      # Hostname section
+  git       # Git section (git_branch + git_status)
+  hg        # Mercurial section (hg_branch  + hg_status)
+  package   # Package version
+  node      # Node.js section
+  ruby      # Ruby section
+  python    # Python section
+  elm       # Elm section
+  elixir    # Elixir section
+  xcode     # Xcode section
+  swift     # Swift section
+  golang    # Go section
+  php       # PHP section
+  rust      # Rust section
+  haskell   # Haskell Stack section
+  java      # Java section
+  julia     # Julia section
+  docker    # Docker section
+  aws       # Amazon Web Services section
+  gcloud    # Google Cloud Platform section
+  venv      # virtualenv section
+  conda     # conda virtualenv section
+  dotnet    # .NET section
+  kubectl   # Kubectl context section
+  terraform # Terraform workspace section
+  ibmcloud  # IBM Cloud section
+  exec_time # Execution time
+  async     # Async jobs indicator
+  line_sep  # Line break
+  battery   # Battery level and status
+  jobs      # Background jobs indicator
+  exit_code # Exit code section
+  char      # Prompt character
+)
+
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="λ"
+SPACESHIP_CHAR_SUFFIX=" "
